fd77c3f31bb37e1e872afb550ec7b084f9ba8875
Nattaphat <eakacit@gmail.com>
2022-03-08 14:12:44 +0700

    commit

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: ['S','M','L','XL'],
             varients:[
                 { id: 2234, color: 'green'},
                 { id: 2235, color: 'blue'}
             ],
-            sizes: ['S','M','L','XL'],
         }
     }
 })
