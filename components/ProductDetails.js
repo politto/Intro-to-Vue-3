d10f2c754de272ce8f0e5f6de825aee0b1353a4d
Nattaphat <eakacit@gmail.com>
2022-03-11 14:44:51 +0700

    commit

---
+++ components/ProductDetails.js
@@ -0,0 +1,31 @@
+app.component('product-detail',{
+    props: {
+        details:{
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `
+    <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    <div class="product-details">
+        <p>Socks for stand user</p>
+      </div>
+    `,
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        
+},
+
+})
+console.log('wtf')
